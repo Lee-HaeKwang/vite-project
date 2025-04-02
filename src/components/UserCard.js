548c15d7ee70dbbef1fa8b5579c45878ecd0fc61
LeeHaeKwang <gorhkd1991@naver.com>
2025-04-02 11:13:36 +0900

    Fixed: 수정

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,8 +1,6 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import { Card, CardContent, Typography } from "@mui/material";
-import { useUserStore } from "../store/useUserStore";
 const UserCard = () => {
-    const { userId, userName } = useUserStore();
-    return (_jsx(Card, { sx: { maxWidth: 300, margin: "20px auto" }, children: _jsxs(CardContent, { children: [_jsx(Typography, { variant: "h6", children: "\uC720\uC800 \uC815\uBCF4" }), _jsxs(Typography, { children: ["ID: ", userId] }), _jsxs(Typography, { children: ["Name: ", userName] })] }) }));
+    return (_jsx(Card, { sx: { maxWidth: 300, margin: "20px auto" }, children: _jsxs(CardContent, { children: [_jsx(Typography, { variant: "h6", children: "\uC720\uC800 \uC815\uBCF4" }), _jsx(Typography, { children: "ID: tester ID" }), _jsx(Typography, { children: "Name: testser NAME" })] }) }));
 };
 export default UserCard;
